b4e00781b2524a05d6d902ff6ce4bcc8601e302d
Noobgam <shessmaster12@gmail.com>
2025-02-15 20:21:57 +0100

    chore: adjust something

--- ui/lib/kv/addon/components/kv-list-filter.js
+++ ui/lib/kv/addon/components/kv-list-filter.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
+import { service } from '@ember/service';
 import { action } from '@ember/object';
 import keys from 'core/utils/key-codes';
 import { keyIsFolder, parentKeyForKey, keyWithoutParentKey } from 'core/utils/key-utils';
